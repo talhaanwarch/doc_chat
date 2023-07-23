c08c3e52a348c4c5ead99af720753138335006bc
Talha <chtalhaanwar@gmail.com>
2023-07-23 23:26:17 +0500

    openai-only-completed

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 docker ps -aq | xargs docker stop | xargs docker rm
 docker volume rm $(docker volume ls -qf dangling=true)
 
-
+docker-compose -f docker-compose.postgres.yml up --build -d
 docker-compose -f docker-compose.milvus.yml up --build -d
 docker-compose -f docker-compose.app.yml up --build -d
 docker-compose -f docker-compose.tooljet.yml up --build -d
